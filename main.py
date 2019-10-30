245895d7dd0b838419adb1452a1d6dff623a7929
inamake <hara.inamake1902@gmail.com>
2019-10-30 09:29:07 +0900

    分岐スイッチの追加(10/30)

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ import os
 
 app = Flask(__name__)
 
+sw0 = False
+sw1 = False
+sw2 = False
+
 #登録データ（リスト）の作成
 name = "not_date"
 postal_code_three_digits = "not_date"
@@ -56,22 +60,23 @@ def callback():
 #アプリ起動を行う。
 @handler.add(MessageEvent, message=TextMessage)
 def start(event):
+    global sw0
     if event.message.text == "喫煙所":
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text="「位置情報」を送ってください。")
+            TextSendMessage(text="『位置情報』を送ってください。")
         )
-        return_postal_code()
+        sw0 = True
     else:
         line_bot_api.reply_message(
             event.reply_token,
-            TextSendMessage(text="「喫煙所」と入力してアプリを起動してください。")
+            TextSendMessage(text="『喫煙所』と入力してアプリを起動してください。")
         )
 
 @handler.add(MessageEvent, message=LocationMessage)
 
 
-# 位置情報から住所、緯度、経度を返す。
+#位置情報から住所、緯度、経度を返す。
 #def return_address(event):
 #    line_bot_api.reply_message(
 #        event.reply_token,
@@ -83,18 +88,27 @@ def start(event):
 
 #位置情報から郵便番号と郵便番号上3桁と住所を返す。
 def return_postal_code(event):
-    Address = event.message.address
-    Postal_code_frist3 = Address[4:7]
-    #postal_code = postal_code_frist3 + address[8:12]
-    line_bot_api.reply_message(
+    global registration_data
+    global sw1
+    if sw0 == True:
+        Address = event.message.address
+        Postal_code_frist3 = Address[4:7]
+        #postal_code = postal_code_frist3 + address[8:12]
+        line_bot_api.reply_message(
+            event.reply_token,
+            [
+                TextSendMessage(text="郵便番号上3桁:\n[{}]\n住所:\n[{}]".format(Postal_code_frist3,Address)),
+            ]
+        )
+
+    else:
+        line_bot_api.reply_message(
         event.reply_token,
-        [
-            TextSendMessage(text="郵便番号上3桁:\n[{}]\n住所:\n[{}]".format(Postal_code_frist3,Address)),
-        ]
-    )
-    registration_data[2] = Address
-    registration_data[1] = Postal_code_frist3
-    print(registration_data)
+        TextSendMessage(text="『位置情報』を送ってください。")
+        )
+        sw1 = True
+        
+
 
 if __name__ == "__main__":
 #    app.run()
